8e19b68c21d1cafff3add434ef7e6cc6191557c2
Murilo barco ianes <muriloianes855@gmail.com>
2026-08-26 11:09:45 -0300

    Add array.js with number mapping and logging

---
+++ array.js
@@ -0,0 +1,16 @@
+// frutas[10] = 'figo';
+
+// console.log(frutas);
+
+//let frutas = ['banana', 'maçã', 'abacaxi', 'perâ'];
+
+//for(let i = 0; i < frutas.length; i++) {
+//  console.log(frutas[i]);}
+
+//frutas.forEach((fruta,i) => console.log(`Fruta[${i}] - ${fruta}`));
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numeros
+    .map((numero) => numero * 2)
+    .forEach((n) => console.log(n));
