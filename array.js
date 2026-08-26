47aca90154808c6345747b188964d8c05a87b899
Murilo barco ianes <muriloianes855@gmail.com>
2026-08-26 11:18:54 -0300

    Refactor array.js to comment out numeros and add precos
    Comment out the original numeros mapping and logging, and add a new precos mapping and filtering operation.

--- array.js
+++ array.js
@@ -9,8 +9,15 @@
 
 //frutas.forEach((fruta,i) => console.log(`Fruta[${i}] - ${fruta}`));
 
-let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-numeros
-    .map((numero) => numero * 2)
-    .forEach((n) => console.log(n));
+//numeros
+//    .map((numero) => numero * 2)
+//    .forEach((n) => console.log(n));
+
+let precos = [10, 20, 30, 40, 50];
+
+precos
+    .map((preco) => preco * 1.10)
+    .filter((valor) => valor < 30)
+    .forEach((preco) => console.log(preco));
